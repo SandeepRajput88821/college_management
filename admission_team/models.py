4c7eaa8791f19a8c4e6f13373c6e82e8e3814f69
rehbarkhan <md.rehbar@outlook.com>
2021-05-02 13:11:51 +0530

    working 1

--- admission_team/models.py
+++ admission_team/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+
+
+class AdmissionTeamDB(models.Model):
+    email = models.CharField(max_length=25, blank=False)
+    password = models.CharField(max_length=35, blank=False)
+    first_name = models.CharField(max_length=15, blank=False)
+    middle_name = models.CharField(max_length=15, blank=True)
+    last_name = models.CharField(max_length=15, blank=False)
+    dob = models.DateField()
+    present_address_street = models.CharField(max_length=15)
+    present_address_city = models.CharField(max_length=15)
+    present_address_zipCode = models.IntegerField(blank=False)
+    present_address_state = models.CharField(max_length=15)
+    permanent_address_street = models.CharField(max_length=15)
+    permanent_address_city = models.CharField(max_length=15)
+    permanent_address_zipCode = models.IntegerField(blank=False)
+    permanent_address_state = models.CharField(max_length=15)
+    mobile_number = models.IntegerField()
+    previous_employment = models.CharField(max_length=20)
+    yearsOfExperience = models.DecimalField(decimal_places=2, max_digits=2)
+    account_status = models.CharField(max_length=20)
+    role = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.email
+
---
+++ rootView/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class RootviewConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'rootView'
